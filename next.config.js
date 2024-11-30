2929755b58a5db55e041798934b9f2bac436dc75
Dehya Qalbi <dehyasproject@gmail.com>
2024-11-30 20:21:26 +0700

    Setup PWA for vercel deployment

--- next.config.js
+++ next.config.js
@@ -9,37 +9,32 @@ const withPWA = require('next-pwa')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compress: true,
   poweredByHeader: false,
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ];
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+      }
+    ]
   },
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: '/(.*)',
         headers: [
           {
             key: 'X-Content-Type-Options',
-            value: 'nosniff',
+            value: 'nosniff'
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
+            value: 'DENY'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = withPWA(nextConfig)
